867200c24797e8466275d84756e2db322cecfda6
Tushar Tyagi <tushar4r@gmail.com>
2017-01-11 18:59:05 +0530

    Updated keywords

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    STT_USERNAME: 'username',
-    STT_PASSWORD: 'password',
-    ALCHEMY_APIKEY: 'alchemy-key'
+    STT_USERNAME: '63e9ac9d-b90a-4ffa-9357-159a4de324d6',
+    STT_PASSWORD: 'llKH5rPn6bPV',
+    ALCHEMY_APIKEY: 'ed0e405ee91e21e319886297c432cfde75e16e7b' 
 }
--- keyword-map.js
+++ keyword-map.js
@@ -1,11 +1,17 @@
-var keywords = [ "rosemary", "granat", "topas", "baseball" ];
+var keywords = [ "rosemary", "granat", "topas",
+                 "baseball", "topaz", "rose mary",
+                 "rose tiles", "rose tile"];
 
 // The location of files wrt to the server root. Each entry corresponds
 // to one entry in `keywords` array above.
 var mappings = {
     'rosemary': 'images/rosemary.jpg',
+    'rose mary': 'images/rosemary.jpg',
+    'rose tile': 'images/rosemary.jpg',
+    'rose tiles': 'images/rosemary.jpg',
     'granat'  : 'images/granat.jpg',
     'topas'   : 'images/topas.jpg',
+    'topaz'   : 'images/topas.jpg',
     'baseball': 'images/baseball.jpg'
 }
 
